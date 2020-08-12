70d5f4a7b8b054ab093806db9d01e226e9645841
lesley <lesleybanadzem2017@gmail.com>
2020-08-12 15:25:02 -0700

    Completed the expensive seq problem

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,23 @@
-# Your code here
+from time import time
 
-
+dictionary = {}
 def expensive_seq(x, y, z):
     # Your code here
+    
+    if x <= 0:
+        return y + z
+    key = f'{x}{y}{z}'
+    if x > 0:
 
+        if key in dictionary:
+
+            return dictionary[key]
+        else:
+            res = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y + 2, z*2) + expensive_seq(x-3, y+3, z*3 )
+
+            dictionary[key] = res
+            return res
+    pass
 
 
 if __name__ == "__main__":
