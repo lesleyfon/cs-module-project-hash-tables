d109f15d183390bc7d8c0eda57178abbce9edd65
lesley <lesleybanadzem2017@gmail.com>
2020-08-15 16:08:02 -0700

    Completed histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,46 @@
 # Your code here
+def print_text(path):
 
+    histo_txt = ""
+    histogram = {}
+    unwanted_char = ['"', ':', ';', ',', '.', ' ,' '-', '+', '=',
+                     '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&', ]
+    for word in open(path,  "r"):
+        word = word.replace("#", "")
+        word = word.replace('"', "")
+        word = word.replace(":", "")
+        word = word.replace(";", "")
+        word = word.replace(",", "")
+        word = word.replace(".", "")
+        word = word.replace("|", "")
+        word = word.replace("-", "")
+        word = word.replace("[", "")
+        word = word.replace("]", "")
+        word = word.replace("{", "")
+        word = word.replace("}", "")
+        word = word.replace("+", "")
+        word = word.replace("=", "")
+        word = word.replace("*", "")
+        word = word.replace("^", "")
+        word = word.replace("&", "")
+        word = word.replace("/", "")
+        word = word.replace('\\', '')
+        word = word.replace('', '')
+        word = word.lower()
+        histo_txt += word
+
+    histo_txt_split_text = histo_txt.split()
+
+    for word in histo_txt_split_text:
+        if word not in histogram:
+            histogram[word] = 1
+        else:
+            histogram[word] += 1
+
+    histogram = sorted(histogram.items(), key=lambda x: x[1], reverse=True)
+
+    for key, value in histogram:
+        print(f'{key}, {value * "#"}')
+
+
+print_text("/Users/lesley/code/Python/Projects/cs-module-project-hash-tables/applications/histo/robin.txt")
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,25 @@
 import random
 
+
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("/Users/lesley/code/Python/Projects/cs-module-project-hash-tables/applications/markov/input.txt", "r") as f:
     words = f.read()
 
+split = words.split()
+dictionary = {}
+
+next = None
+
+for word in split:
+    if word not in dictionary:
+        dictionary[word] = []
+
+for i in range(len(split)-1):
+    current_word = split[i]
+    dictionary[current_word].append(split[i + 1])
 # TODO: analyze which words can follow other words
 # Your code here
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
